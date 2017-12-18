8dd8f0d47ce2f007492749c34585321cc6d1236f
b-rohit <rohitbohra9@gmail.com>
2017-12-18 10:29:24 +0100

    change format of output .net file to match GIS

--- linearInterGIS.py
+++ linearInterGIS.py
@@ -1,19 +1,18 @@
-import xlsxwriter
 import random
 import sys
 
 #function to calculate intermediate points on a linear line between two points
 #it takes a polyline points as list of list, rFactor to randomize interval as inputs and
 #output results in a xlsx file
-def interpolate(data, interval, rFactor, row, worksheet):
+def interpolate(data, interval, rFactor, row,fh):
     i = 0
     start = data[0][0] + interval
     while i < len(data) -1:
         slope = (data[i+1][1] - data[i][1])/(data[i+1][0] - data[i][0])
         while start  < data[i+1][0]:
             ycord = data[i][1] + slope*(start - data[i][0])
-            worksheet.write(row, 0,     start)
-            worksheet.write(row, 1, ycord)
+            strl = "1000;" + str(row) +";;;;;0;" + str(start) + ";" + str(ycord) + ";0;10.000;0;0\n";
+            fh.write(strl)
             row = row +1
             if rFactor:
                 start = start + interval*(1 + random.uniform(-0.5, 0.5))
@@ -24,11 +23,13 @@ def interpolate(data, interval, rFactor, row, worksheet):
     return row
 
 def linearInterpolate(fPath, interval, rFactor):
-    workbook = xlsxwriter.Workbook('interpolateddata.xlsx')
-    worksheet = workbook.add_worksheet()
     #file handler for file
     f = open(fPath, "r")
-    row = 0
+    index=1
+    fh = open("output.net", "w")
+    header ="$VISION\n* Universitaet Stuttgart Fakultaet 2 Bau+Umweltingenieurwissenschaften Stuttgart\n* 12/14/17\n*\n* Table: Version block\n*\n$VERSION:VERSNR;FILETYPE;LANGUAGE;UNIT\n10.000;Net;ENG;KM\n\n*\n* Table: POI categories\n*\n$POICATEGORY:NO;CODE;NAME;COMMENT;PARENTCATNO\n1000;LP;LinkPoints;;0\n\n*\n* Table: Points of interest: LinkPoints (1000)\n*\n$POIOFCAT_1000:CATNO;NO;CODE;NAME;COMMENT;IMAGEFILENAME;USEIMAGEFILE;XCOORD;YCOORD;SURFACEID;IMAGEHEIGHT;USEIMAGEHEIGHT;IMAGEANGLE\n"
+    fh.write(header)
+    row = 1
     # loop through a file and read a line at a time
     for line in f:
         #read between paranthesis and convert to a list of strings
@@ -40,8 +41,8 @@ def linearInterpolate(fPath, interval, rFactor):
             data.append([float(d[i]), float(d[i+1])])
             i+=2;
         #function call to calculate intermediate points
-        row = interpolate(data, interval, rFactor, row, worksheet)
+        row = interpolate(data, interval, rFactor, row, fh)
     print "Completed Successfully"
     f.close()
-    workbook.close()
+    fh.close()
 
